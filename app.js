0c2807c704b773fe3a45de009f9eb3d0c8800c16
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2025-01-26 20:36:46 -0300

    javascript do desafio

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const nomes = [];
+
+function adicionarAmigo() {
+    const inputNome = document.querySelector("input");
+    const nomeAdicionado = inputNome.value.trim();
+    const listaDosNomes = document.getElementById("listaAmigos");
+
+    if (nomeAdicionado) {
+        nomes.push(nomeAdicionado);
+        atualizarLista(listaDosNomes);
+        inputNome.value = '';
+    } else {
+        alert("Por favor, insira um nome válido!");
+    }
+}
+
+function atualizarLista(listaDosNomes) {
+    listaDosNomes.innerHTML = nomes.map(nome => `<li>${nome}</li>`).join('');
+}
+
+function sortearAmigo() {
+    if (nomes.length === 0) {
+        alert("A lista de amigos está vazia! Adicione nomes antes de sortear.");
+        return;
+    }
+
+    const nomeEscolhido = nomes[Math.floor(Math.random() * nomes.length)];
+    const sortear = document.getElementById("resultado");
+    sortear.innerHTML = nomeEscolhido;
+}
